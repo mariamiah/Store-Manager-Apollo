88a81d7c4e622e17ee3d3e117a6424357d00a127
mariamiah <mariah.kakande@gmail.com>
2021-04-07 18:54:13 +0300

    update README file

--- src/graphql/query.js
+++
@@ -1,6 +0,0 @@
-function feed() {
-    const { loading, error, data } = useQuery(GET_DOGS);
-    if(error) return <Error />
-    if(loading || !data) return <Fetching />
-    return <DogList dogs={data.dogs} />
-}
